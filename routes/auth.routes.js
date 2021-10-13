f7af702dd71ea464e22ceeae87a969ff2fbeff1d
Julian DAquila <jdaquila@gmail.com>
2021-10-13 06:57:11 +0200

    auth routers

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,13 +2,13 @@ const router = require('express').Router()
 const authController = require('../controllers/auth.controllers')
 const { isAuthenticated } = require('../middleware/jwt.middleware.js')
 
-// GET  /auth/verify  -  Used to verify JWT stored on the client
-router.get('/', isAuthenticated, authController.verify)
+// POST /auth/signup  - Creates a new user in the database
+router.post('/signup', authController.signup)
 
 // POST  /auth/login - Verifies email and password and returns a JWT
 router.post('/login', authController.login)
 
-// POST /auth/signup  - Creates a new user in the database
-router.post('/signup', authController.signup)
+// GET  /auth/verify  -  Used to verify JWT stored on the client
+router.get('/verify', isAuthenticated, authController.verify)
 
 module.exports = router
